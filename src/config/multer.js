d89f4aeef5238a53773c210ffc896e2ad27bf9b8
Joao C Barbosa <joaao@outlook.com>
2021-01-25 15:27:23 -0300

    fix: remove error

--- src/config/multer.js
+++ src/config/multer.js
@@ -1,33 +1,31 @@
-const multer = require('multer');
-const path = require('path');
-const crypto = require('crypto');
-const aws = require('aws-sdk');
-const multerS3 = require('multer-s3');
-
+const multer = require("multer");
+const path = require("path");
+const crypto = require("crypto");
+const aws = require("aws-sdk");
+const multerS3 = require("multer-s3");
 
 const MAX_SIZE_TWO_MEGABYTES = 2 * 1024 * 1024;
 
-const storages = {
+const storageTypes = {
     local: multer.diskStorage({
         destination: (req, file, cb) => {
-            cb(null, path.resolve(__dirname, '..', '..', 'tmp', 'uploads'));
+            cb(null, path.resolve(__dirname, "..", "..", "tmp", "uploads"));
         },
         filename: (req, file, cb) => {
             crypto.randomBytes(16, (err, hash) => {
-                if (err) {
-                    cb(err);
-                }
+                if (err) cb(err);
 
-                file.key = `${hash.toString('hex')}-${file.originalname}`;
+                file.key = `${hash.toString("hex")}-${file.originalname}`;
 
                 cb(null, file.key);
             });
-        }
+        },
     }),
     s3: multerS3({
+        s3: new aws.S3(),
         bucket: process.env.BUCKET_NAME,
         contentType: multerS3.AUTO_CONTENT_TYPE,
-        acl: 'public-read',
+        acl: "public-read",
         key: (req, file, cb) => {
             crypto.randomBytes(16, (err, hash) => {
                 if (err) cb(err);
@@ -36,14 +34,13 @@ const storages = {
 
                 cb(null, fileName);
             });
-        }
-    })
-}
-
+        },
+    }),
+};
 
 module.exports = {
     dest: path.resolve(__dirname, "..", "..", "tmp", "uploads"),
-    storage: storages[process.env.STORAGE_TYPE],
+    storage: storageTypes[process.env.STORAGE_TYPE],
     limits: {
         fileSize: MAX_SIZE_TWO_MEGABYTES,
     },
